f562051bf3d035062f1edce689d082c7ec1892cf
Theo Proença <theoproenca@gmail.com>
2023-04-12 16:23:47 -0300

    ajuste nos dias

--- 06. js moderno/ex016/script.js
+++ 06. js moderno/ex016/script.js
@@ -6,7 +6,7 @@ function birthday(date){
     
     const diff = today.diff(birthday, 'year')
     const next = birthday.add(diff + 1, 'year').format('DD/MM/YYYY')
-    const nextDiff = dayjs(next).diff(dayjs(), 'day')
+    const nextDiff = dayjs(next).diff(dayjs(), 'day') + 1
 
     console.log(`Você tem ${diff} anos, seu próximo aniversário é em ${next} e faltam ${nextDiff} dias.`)
 }
