7d04be40c10e79ac1b8ec9c99e20decd2839b6ad
Theo Proença <theoproenca@gmail.com>
2023-04-12 16:23:10 -0300

    ex016 - dayjs

---
+++ 06. js moderno/ex016/script.js
@@ -0,0 +1,15 @@
+const dayjs = require("dayjs")
+
+function birthday(date){
+    const birthday = dayjs(date)
+    const today = dayjs()
+    
+    const diff = today.diff(birthday, 'year')
+    const next = birthday.add(diff + 1, 'year').format('DD/MM/YYYY')
+    const nextDiff = dayjs(next).diff(dayjs(), 'day')
+
+    console.log(`Você tem ${diff} anos, seu próximo aniversário é em ${next} e faltam ${nextDiff} dias.`)
+}
+
+
+birthday("1997-11-10")
