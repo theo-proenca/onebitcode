b05a6691c7180fa4bec8965ab87fa6ba66fa6b91
Theo Proença <theoproenca@gmail.com>
2023-04-13 23:21:24 -0300

    objects - review

---
+++ 07. js - poo/revisão/index.js
@@ -0,0 +1,36 @@
+/* const book = {
+    title: 'Eragon',
+    pages: 468,
+    published: true,
+    inStock: 20,
+    tags: ['fantasy', 'adventure', 'medieval'],
+    author: {
+        name: 'Christopher Paolini'
+    },
+    addOnStock(quantity) {
+        this.inStock += quantity
+    },
+} */
+
+function Book(title, pages, tags, author){
+    this.title = title
+    this.pages = pages
+    this.tags = tags
+    this.author = author
+    this.published = false
+    this.inStock = 0
+    this.addOnStock = function addOnStock(quantity) {
+        this.inStock += quantity
+    }
+    this.save = function () {
+        // salva no banco de dados
+    }
+}
+const author = { name: "Christopher Paolini"}
+const tags = ["fantasy", "adventure"]
+
+const eragon = new Book("Eragon", 468, tags, author)
+console.log(eragon)
+
+const eldest = new Book("Eldest", 644, tags, author)
+console.log(eldest)
