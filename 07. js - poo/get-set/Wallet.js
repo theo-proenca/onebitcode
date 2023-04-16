9f432b395d406fc8b94e392c60149edda437a132
Theo Proença <theoproenca@gmail.com>
2023-04-15 22:36:14 -0300

    get&set

---
+++ 07. js - poo/get-set/Wallet.js
@@ -0,0 +1,28 @@
+class Wallet {
+    #amount
+    #username
+
+    constructor() {
+        this.#amount = 100.99 * 100
+    }
+
+    get amount() {
+        return this.#amount / 100
+    }
+
+    set username(newUsername) {
+        this.#username = newUsername
+    }
+
+    get username() {
+        return this.#username
+    }
+}
+
+const myWallet = new Wallet()
+
+console.log(myWallet.amount)
+
+myWallet.username = 'Dibaz'
+console.log(myWallet.username)
+
